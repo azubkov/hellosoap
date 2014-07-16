007b7158348f16be43fca814af00b5dc04210d0e
azubkov <artem.zubkoff@gmail.com>
2014-07-16 17:49:52 +0300

    Utils: ToSentences method added

--- src/main/java/github/com/azubkov/hellosoap/utils/Utils.java
+++ src/main/java/github/com/azubkov/hellosoap/utils/Utils.java
@@ -7,6 +7,10 @@ import com.google.common.io.Resources;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.text.BreakIterator;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public final class Utils {
     private Utils() {
@@ -20,6 +24,26 @@ public final class Utils {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static List<String> toSentences(String string) {
+        BreakIterator bi = BreakIterator.getSentenceInstance(Locale.US);
+        bi.setText(string);
+        List<String> list = new ArrayList<String>();
+        int lastIndex = bi.first();
+        while (lastIndex != BreakIterator.DONE) {
+            int firstIndex = lastIndex;
+            lastIndex = bi.next();
+            if (lastIndex != BreakIterator.DONE) {
+                String sentence = string.substring(firstIndex, lastIndex);
+                list.add(sentence);
+            }
+        }
+        List<String> filtered = new ArrayList<String>();
+        for (String s : list) {
+            filtered.add(s.trim());
+        }
+        return filtered;
     }
 
 }
--- src/test/java/github/com/azubkov/hellosoap/utils/UtilsTest.java
+++ src/test/java/github/com/azubkov/hellosoap/utils/UtilsTest.java
@@ -5,6 +5,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import java.util.List;
+
 public class UtilsTest {
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
@@ -21,5 +23,18 @@ public class UtilsTest {
         String string = Utils.getResource("github/com/azubkov/hellosoap/document/FarFarAway.txt");
         Assert.assertNotNull(string);
         Assert.assertTrue(org.apache.commons.lang3.StringUtils.containsIgnoreCase(string, "far far away"));
+    }
+
+    @Test
+    public void testToSentences() {
+        List<String> list = Utils.toSentences("Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.\n" +
+                "\n" +
+                " Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.\n" +
+                "\n" +
+                " A small river named Duden flows by their place and supplies it with the necessary regelialia.");
+        Assert.assertNotNull(list);
+        Assert.assertEquals(list.get(0), "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.");
+        Assert.assertEquals(list.get(1), "Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.");
+        Assert.assertEquals(list.get(2), "A small river named Duden flows by their place and supplies it with the necessary regelialia.");
     }
 }
